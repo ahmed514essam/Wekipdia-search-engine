74d152e8df121bd53177c18b391dd2f525c0504f
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-02-25 02:46:01 +0200

    version1

---
+++ src/pages/Home/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import SearchItem from "../SearchItem/SearchItem";
+import { getArticlesFromLocalStorage } from "../../../../utils";
+
+export default function SearchResults({ results }) {
+  const [favouriteArticles, setFavouriteArticles] = useState(
+    getArticlesFromLocalStorage()
+  );
+  const favouriteIDs = favouriteArticles.map((article) => article.pageid);
+
+  useEffect(() => {
+    localStorage.setItem("articles", JSON.stringify(favouriteArticles));
+  }, [favouriteArticles]);
+
+  if (results.length === 0) {
+    return <p>No results here, Try Searching for Something....</p>;
+  }
+
+  return (
+    <div>
+      {results.map((result) => (
+        <SearchItem
+          key={result.pageid}
+          title={result.title}
+          snippet={result.snippet}
+          pageid={result.pageid}
+          setFavouriteArticles={setFavouriteArticles}
+          isFavourite={favouriteIDs.includes(result.pageid)}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/pages/ScrollToTopButton/ScrollTop.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import style from "./Scroll.module.css";
+export default function ScrollTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight / 2;
+      setIsVisible(isBottom);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 1,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      {isVisible && (
+        <button className={style.atom} onClick={scrollToTop}>
+          <img src="/public/download-removebg-preview (1).png" />
+        </button>
+      )}
+    </div>
+  );
+}
